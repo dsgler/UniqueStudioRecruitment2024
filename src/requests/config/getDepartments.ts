6eb7fe70d373c529a45b6eed616ecbf3710e93f2
HUST-SE-LY <1596087252@qq.com>
2025-03-02 19:40:50 +0800

    feat: update

--- src/requests/config/getDepartments.ts
+++ src/requests/config/getDepartments.ts
@@ -3,5 +3,5 @@ import { http } from "../http";
 // ly: whiteboard nodes type: https://open.feishu.cn/document/ukTMukTMukTM/uUDN04SN0QjL1QDN/board-v1/whiteboard-node/list?appId=cli_a74bda9b7218500d
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const getDepartments = () => http.get<any>("/config/whiteboard", {
-  whiteboard_id: "OiAWws7ZKhXIAkbT7Qvc1btJnwc"
+  whiteboard_id: "OiAWws7ZKhXIAkbT7Qvc1btJnwc",
 });
